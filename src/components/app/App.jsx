4b26fedd626c0547e7439cb3cb4d4b8909618e0c
Marensfc <alexeymaren12@gmail.com>
2024-03-31 14:45:05 +0300

    Create project

---
+++ src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <h1>Phonebook</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,7 @@
+import css from "./Contact.module.css";
+
+const Contact = () => {
+  return <div></div>;
+};
+
+export default Contact;
